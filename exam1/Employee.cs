cde2812ef546f7ad87e84d67311d038a68c19bd6
zmil44 <zachary.j.miller@outlook.com>
2018-08-14 12:33:10 -0400

    initial commit

---
+++ exam1/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam1
+{
+    class Employee
+    {
+      
+        private double salesWeekly;
+        private double COMMISSION = .07;
+        private double FEDERAL_TAX = .18;
+        private double SOCIAL_SECURITY = .06;
+        private double grossPay;
+        private double federalTax;
+        private double socialSecurity;
+        private double netPay;
+
+        public Employee() 
+        { 
+        }
+
+        public Employee(double weeklySales)
+        {
+            salesWeekly=weeklySales;
+        }
+            public double GrossPay()
+             {
+                grossPay = (salesWeekly * COMMISSION);
+                return grossPay;
+             }
+            public double CalculateFederalTax(double payGross)
+            {
+                federalTax = (payGross * FEDERAL_TAX);
+                return federalTax;
+            }
+            public double CalculateSocialSecurity(double payGross)
+            {
+                socialSecurity=(payGross*SOCIAL_SECURITY);
+                return socialSecurity;
+            }
+            public double NetPay(double payGross,double socialSecurityTax,double federalTax)
+            {
+            
+                netPay=(payGross-(socialSecurityTax+federalTax));
+                return netPay;
+            }
+    } 
+}
+
---
+++ exam1/EmployeeApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam1
+{
+    class EmplyoeeApp
+    {
+        static void Main(string[] args)
+        {
+            
+            string firstName = "John";
+            string lastName = "Doe";
+            double weeklySales = 24755.95;
+            double federalTax, socialSecurityTax, payGross, payNet;
+            Console.WriteLine("Weekly information for " + firstName + " " + lastName);
+            Console.WriteLine("Sales: " + "{0:c2}", weeklySales);
+            Employee newEmployee = new Employee(weeklySales);
+            payGross = newEmployee.GrossPay();
+            federalTax=newEmployee.CalculateFederalTax(payGross);
+            socialSecurityTax=newEmployee.CalculateSocialSecurity(payGross);
+            payNet=newEmployee.NetPay(payGross,socialSecurityTax,federalTax);
+            DisplayResults(payGross, federalTax, socialSecurityTax, payNet);
+
+            
+        }
+        public static void DisplayResults(double payGross,double federalTax,double socialSecurityTax,double payNet)
+        {
+            Console.WriteLine("\nGross Pay: " + "{0:c2}", payGross);
+            Console.WriteLine("Federal Tax: " + "{0:c2}", federalTax);
+            Console.WriteLine("Social Security: " + "{0:c2}", socialSecurityTax);
+            Console.WriteLine("Take Home Pay: " + "{0:c2}", payNet);
+        }
+
+    }
+}
